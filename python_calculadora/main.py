b0d5b6d82efa70eb6047cb406ea0c28c52ee49aa
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-18 12:29:47 -0300

    adicionando botoes de operacoes e segunda fileira

--- python_calculadora/main.py
+++ python_calculadora/main.py
@@ -17,8 +17,17 @@ e.grid(
     pady=2
 )
 #function operation
+def botao_click():
+    return
 def botao_adicao():
     return
+def botao_subtracao():
+    return
+def botao_multiplicacao():
+    return
+def botao_divisao():
+    return
+#botoes de operacoes
 adicao = Button(root,
                 text='+',
                 padx=40,
@@ -32,9 +41,46 @@ adicao = Button(root,
                 font=('futura', 12, 'bold')
 )
 adicao.grid(row=0, column=4)
+subtracao = Button(root,
+                text='-',
+                padx=40,
+                pady=20,
+                command=botao_subtracao,
+                fg='#FFFFFF',
+                activeforeground='#FFFFFF',
+                bg='#320064',
+                activebackground='#240086',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+subtracao.grid(row=1, column=4)
+multiplicacao = Button(root,
+                text='*',
+                padx=40,
+                pady=20,
+                command=botao_multiplicacao,
+                fg='#FFFFFF',
+                activeforeground='#FFFFFF',
+                bg='#320064',
+                activebackground='#240086',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+multiplicacao.grid(row=2, column=4)
+divisao = Button(root,
+                text='/',
+                padx=40,
+                pady=20,
+                command=botao_divisao,
+                fg='#FFFFFF',
+                activeforeground='#FFFFFF',
+                bg='#320064',
+                activebackground='#240086',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+divisao.grid(row=3, column=4)
 #primeira fileira
-def botao_click():
-    return
 um = Button(root,
             text='1',
             padx=40,
@@ -47,7 +93,7 @@ um = Button(root,
             relief=FLAT,
             font=('futura', 12, 'bold')
 )
-adicao.grid(row=1, column=1)
+um.grid(row=1, column=1)
 
 dois = Button(root,
             text='2',
@@ -61,7 +107,7 @@ dois = Button(root,
             relief=FLAT,
             font=('futura', 12, 'bold')
 )
-adicao.grid(row=1, column=2)
+dois.grid(row=1, column=2)
 
 tres = Button(root,
             text='3',
@@ -75,6 +121,49 @@ tres = Button(root,
             relief=FLAT,
             font=('futura', 12, 'bold')
 )
-adicao.grid(row=1, column=3)
+tres.grid(row=1, column=3)
+
+#segunda fileira
+quatro = Button(root,
+            text='4',
+            padx=40,
+            pady=20,
+            command=lambda: botao_click(4),
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+quatro.grid(row=2, column=1)
+
+cinco = Button(root,
+            text='5',
+            padx=40,
+            pady=20,
+            command=lambda: botao_click(5),
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+cinco.grid(row=2, column=2)
+
+seis = Button(root,
+            text='6',
+            padx=40,
+            pady=20,
+            command=lambda: botao_click(6),
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+seis.grid(row=2, column=3)
 root.mainloop()
 
