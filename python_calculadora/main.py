932db9f34f810dbb5c3595f1063e3a2bb35034b3
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-20 23:56:10 -0300

    interface completa

--- python_calculadora/main.py
+++ python_calculadora/main.py
@@ -9,7 +9,7 @@ root.maxsize(480,360)
 root.configure(background='#282828')
 
 e = Entry(root, width=15, borderwidth=4, relief=FLAT, fg='#FFFFFF', bg='#a7a28f', 
-font=('futura', 25, 'bold'), justify=CENTER,)
+font=('futura', 25, 'bold'), justify=CENTER)
 e.grid(
     row=0,
     column=0,
@@ -26,6 +26,10 @@ def botao_subtracao():
 def botao_multiplicacao():
     return
 def botao_divisao():
+    return
+def botao_limpar():
+    return
+def botao_igual():
     return
 #botoes de operacoes
 adicao = Button(root,
@@ -165,5 +169,86 @@ seis = Button(root,
             font=('futura', 12, 'bold')
 )
 seis.grid(row=2, column=3)
+#terceira fileira
+sete = Button(root,
+            text='7',
+            padx=50,
+            pady=20,
+            command=lambda: botao_click(7),
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+sete.grid(row=3, column=1)
+oito = Button(root,
+            text='8',
+            padx=50,
+            pady=20,
+            command=lambda: botao_click(8),
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+oito.grid(row=3, column=2)
+nove = Button(root,
+            text='9',
+            padx=50,
+            pady=20,
+            command=lambda: botao_click(9),
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+nove.grid(row=3, column=3)
+#quarta fileira
+zero = Button(root,
+            text='0',
+            padx=110,
+            pady=20,
+            command=lambda: botao_click(0),
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+zero.grid(row=4, column=1, columnspan=2)
+apagar = Button(root,
+            text='C',
+            padx=50,
+            pady=20,
+            command=botao_limpar,
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+apagar.grid(row=4, column=3)
+igual = Button(root,
+            text='=',
+            padx=50,
+            pady=20,
+            command=botao_igual,
+            fg='#FFFFFF',
+            activeforeground='#FFFFFF',
+            bg='#320064',
+            activebackground='#240086',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+)
+igual.grid(row=4, column=4)
+
 root.mainloop()
 
