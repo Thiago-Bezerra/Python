c249f8cd05ec92eaea116d1c42c695b9a55cd765
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-02-21 01:18:46 -0300

    finalizando logica da calculadora

--- python_calculadora/main.py
+++ python_calculadora/main.py
@@ -8,6 +8,14 @@ root.maxsize(480,360)
 
 root.configure(background='#282828')
 
+# variaveis globais
+numero1 = ""
+numero2 = ""
+adicao = FALSE
+subtracao = FALSE
+divisao = FALSE
+multiplicacao = FALSE
+
 e = Entry(root, width=15, borderwidth=4, relief=FLAT, fg='#FFFFFF', bg='#a7a28f', 
 font=('futura', 25, 'bold'), justify=CENTER)
 e.grid(
@@ -16,38 +24,90 @@ e.grid(
     columnspan=4,
     pady=2
 )
-#function operation
-def botao_click():
-    return
+# function operation
+
+
+def botao_click(num):
+    e.insert(END, num)
+
+
 def botao_adicao():
-    return
+    global numero1
+    global adicao
+    adicao = True
+    numero1 = e.get()
+    e.delete(0, END)
+
+
 def botao_subtracao():
-    return
+    global numero1
+    global subtracao
+    subtracao = True
+    numero1 = e.get()
+    e.delete(0, END)
+
+
 def botao_multiplicacao():
-    return
+    global numero1
+    global multiplicacao
+    multiplicacao = True
+    numero1 = e.get()
+    e.delete(0, END)
+
+
 def botao_divisao():
-    return
+    global numero1
+    global divisao
+    divisao = True
+    numero1 = e.get()
+    e.delete(0, END)
+
+
 def botao_limpar():
-    return
+    e.delete(0, END)
+
+
 def botao_igual():
-    return
-#botoes de operacoes
-adicao = Button(root,
+    global adicao
+    global subtracao
+    global multiplicacao
+    global divisao
+
+    numero2 = e.get()
+    e.delete(0, END)
+    if adicao == TRUE:
+        e.insert(0, int(numero1)+int(numero2))
+        adicao = FALSE
+    if subtracao == TRUE:
+        e.insert(0, int(numero1)-int(numero2))
+        subtracao = FALSE
+    if multiplicacao == TRUE:
+        e.insert(0, int(numero1)*int(numero2))
+        multiplicacao = FALSE
+    if divisao == TRUE:
+        e.insert(0, int(numero1)/int(numero2))
+        divisao = FALSE
+
+
+# botoes de operacoes
+adicao = Button(
+                root,
                 text='+',
-                padx=40,
+                padx=50,
                 pady=20,
-                command=botao_adicao,
+                command = botao_adicao,
                 fg='#FFFFFF',
                 activeforeground='#FFFFFF',
                 bg='#320064',
-                activebackground='#240086',
-                relief=FLAT,
+                 activebackground='#240086',
+                relief= FLAT,
                 font=('futura', 12, 'bold')
 )
 adicao.grid(row=0, column=4)
-subtracao = Button(root,
+subtracao = Button (
+                root,
                 text='-',
-                padx=40,
+                padx=50,
                 pady=20,
                 command=botao_subtracao,
                 fg='#FFFFFF',
@@ -58,9 +118,10 @@ subtracao = Button(root,
                 font=('futura', 12, 'bold')
 )
 subtracao.grid(row=1, column=4)
-multiplicacao = Button(root,
+multiplicacao = Button(
+                root,
                 text='*',
-                padx=40,
+                padx=50,
                 pady=20,
                 command=botao_multiplicacao,
                 fg='#FFFFFF',
@@ -71,9 +132,10 @@ multiplicacao = Button(root,
                 font=('futura', 12, 'bold')
 )
 multiplicacao.grid(row=2, column=4)
-divisao = Button(root,
+divisao = Button(
+                root,
                 text='/',
-                padx=40,
+                padx=50,
                 pady=20,
                 command=botao_divisao,
                 fg='#FFFFFF',
@@ -85,9 +147,10 @@ divisao = Button(root,
 )
 divisao.grid(row=3, column=4)
 #primeira fileira
-um = Button(root,
+um = Button(
+            root,
             text='1',
-            padx=40,
+            padx=50,
             pady=20,
             command=lambda: botao_click(1),
             fg='#FFFFFF',
@@ -99,9 +162,10 @@ um = Button(root,
 )
 um.grid(row=1, column=1)
 
-dois = Button(root,
+dois = Button(
+            root,
             text='2',
-            padx=40,
+            padx=50,
             pady=20,
             command=lambda: botao_click(2),
             fg='#FFFFFF',
@@ -113,64 +177,69 @@ dois = Button(root,
 )
 dois.grid(row=1, column=2)
 
-tres = Button(root,
+tres = Button(
+            root,
             text='3',
-            padx=40,
+            padx=50,
             pady=20,
             command=lambda: botao_click(3),
             fg='#FFFFFF',
             activeforeground='#FFFFFF',
             bg='#320064',
             activebackground='#240086',
-            relief=FLAT,
+            relief= FLAT,
             font=('futura', 12, 'bold')
 )
 tres.grid(row=1, column=3)
 
-#segunda fileira
-quatro = Button(root,
+# segunda fileira
+quatro = Button(
+            root,
             text='4',
-            padx=40,
+            padx=50,
             pady=20,
             command=lambda: botao_click(4),
             fg='#FFFFFF',
             activeforeground='#FFFFFF',
             bg='#320064',
             activebackground='#240086',
-            relief=FLAT,
+            relief= FLAT,
             font=('futura', 12, 'bold')
 )
 quatro.grid(row=2, column=1)
 
-cinco = Button(root,
+cinco = Button(
+            root,
             text='5',
-            padx=40,
+            padx=50,
             pady=20,
             command=lambda: botao_click(5),
             fg='#FFFFFF',
             activeforeground='#FFFFFF',
             bg='#320064',
             activebackground='#240086',
-            relief=FLAT,
+            relief= FLAT,
             font=('futura', 12, 'bold')
 )
 cinco.grid(row=2, column=2)
 
-seis = Button(root,
+seis = Button(
+            root,
             text='6',
-            padx=40,
+            padx=50,
             pady=20,
             command=lambda: botao_click(6),
             fg='#FFFFFF',
             activeforeground='#FFFFFF',
             bg='#320064',
             activebackground='#240086',
-            relief=FLAT,
+            relief= FLAT,
             font=('futura', 12, 'bold')
 )
 seis.grid(row=2, column=3)
-#terceira fileira
-sete = Button(root,
+# terceira fileira
+sete = Button(
+            root,
             text='7',
             padx=50,
             pady=20,
@@ -183,7 +252,8 @@ sete = Button(root,
             font=('futura', 12, 'bold')
 )
 sete.grid(row=3, column=1)
-oito = Button(root,
+oito = Button(
+            root,
             text='8',
             padx=50,
             pady=20,
@@ -196,7 +266,8 @@ oito = Button(root,
             font=('futura', 12, 'bold')
 )
 oito.grid(row=3, column=2)
-nove = Button(root,
+nove = Button(
+            root,
             text='9',
             padx=50,
             pady=20,
@@ -209,8 +280,9 @@ nove = Button(root,
             font=('futura', 12, 'bold')
 )
 nove.grid(row=3, column=3)
-#quarta fileira
-zero = Button(root,
+# quarta fileira
+zero = Button(  
+            root,
             text='0',
             padx=110,
             pady=20,
@@ -223,7 +295,8 @@ zero = Button(root,
             font=('futura', 12, 'bold')
 )
 zero.grid(row=4, column=1, columnspan=2)
-apagar = Button(root,
+apagar = Button(
+            root,
             text='C',
             padx=50,
             pady=20,
@@ -236,7 +309,8 @@ apagar = Button(root,
             font=('futura', 12, 'bold')
 )
 apagar.grid(row=4, column=3)
-igual = Button(root,
+igual = Button(
+            root,
             text='=',
             padx=50,
             pady=20,
