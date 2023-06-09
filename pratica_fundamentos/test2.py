087e056f552deb2da364cf66b76a05ccf2b78f9b
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-06-09 17:22:24 -0300

    praticando fundamento python

---
+++ pratica_fundamentos/test2.py
@@ -0,0 +1,34 @@
+# N = int(input())
+
+# for i in range(N):
+    # A, B = input().strip().split(' ')
+
+    # if(len(B) > len(A)):
+        # print('nao encaixa')
+    # else:
+        # A = A[(len(A) - len(B)):]
+        # print('encaixa' if A == B else 'nao encaixa')
+
+
+
+
+N = int(input())
+
+while(N > 0):
+    # A = int(input())
+    # B = int(input())
+    A, B = input().strip().split(' ')
+    if int(A) >= int(B) and (int(A) - int(B))%10 == 0:
+        print("encaixa")
+    else:
+        print("nao encaixa")
+    N -=1
+
+# qte = int(input())
+
+# for i in range(qte):
+    # A, B = list(map(str,input().split()))
+    # if A[-len(B):] == B:
+        # print ("encaixa")
+    # else:
+        # print("nao encaixa")
