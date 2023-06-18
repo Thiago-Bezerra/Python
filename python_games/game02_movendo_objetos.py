6bdfeb3eb993c729fcf5f02a0f5764665e2afb59
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-06-18 17:21:22 -0300

    movendo objetos

--- python_games/game02_movendo_objetos.py
+++ python_games/game02_movendo_objetos.py
@@ -7,18 +7,26 @@ pygame.init()
 
 altura = 480
 largura = 640
+eixo_x = largura/2
+eixo_y = 0
+
 
 # DEFINIR O TAMANHO DA JANELA E O NOME DA JANELA DO JOGO
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo")
+relogio = pygame.time.Clock()
+
 
 # LOOP DA OCORRENCIA DO JOGO ENQUANTO ABERTO
 while True:
+    relogio.tick(20)
+    tela.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(tela, (255, 0, 0), (200, 300, 40, 50))
-    pygame.draw.circle(tela, (0, 0, 120), (300, 260), 40)
-    pygame.draw.line(tela, (255, 255, 0), (390, 0), (390, 600), 5)
+    pygame.draw.rect(tela, (255, 0, 0), (eixo_x, eixo_y, 40, 50))
+    if eixo_y >= altura:
+        eixo_y = 0
+    eixo_y += 5
     pygame.display.update()
