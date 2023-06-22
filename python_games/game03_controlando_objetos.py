e5e455c373b48eb9c4da2e36e3cc269e5ca5003c
Thiago-Bezerra <thiagodesouzabezerra@gmail.com>
2023-06-21 22:09:05 -0300

    adicionando controle de movimento no objeto

--- python_games/game03_controlando_objetos.py
+++ python_games/game03_controlando_objetos.py
@@ -7,15 +7,32 @@ pygame.init()
 
 altura = 480
 largura = 640
+eixo_x = largura/2
+eixo_y = altura/2
+
 
 # DEFINIR O TAMANHO DA JANELA E O NOME DA JANELA DO JOGO
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo")
+relogio = pygame.time.Clock()
+
 
 # LOOP DA OCORRENCIA DO JOGO ENQUANTO ABERTO
 while True:
+    relogio.tick(20)
+    tela.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+    if pygame.key.get_pressed()[pygame.K_a]:
+        eixo_x -= 20
+    if pygame.key.get_pressed()[pygame.K_d]:
+        eixo_x += 20
+    if pygame.key.get_pressed()[pygame.K_w]:
+        eixo_y -= 20
+    if pygame.key.get_pressed()[pygame.K_s]:
+        eixo_y += 20
+
+    pygame.draw.rect(tela, (255, 0, 0), (eixo_x, eixo_y, 40, 50))
     pygame.display.update()
